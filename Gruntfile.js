c2662d7e1679b449e42bc7481a79f0f5c4b4e429
viitorzzo <vitornunesemailcelular@gmail.com>
2025-07-02 22:07:10 -0300

    Corrigido Gruntfile.js e tarefas de LESS, CSS e JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,59 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        less: {
-            development: {
-                files: {
-                    'dist/styles.css': 'src/styles.less'
-                }
-            }
-        },
-
-        cssmin: {
-            target: {
-                files: {
-                    'dist/styles.min.css': ['dist/styles.css']
-                }
-            }
-        },
-
-        uglify: {
-            my_target: {
-                files: {
-                    'dist/scripts.min.js': ['src/scripts.js']
-                }
-            }
-        },
-
-        watch: {
-            styles: {
-                files: ['src/*.less'],
-                tasks: ['less', 'cssmin'],
-                options: {
-                    spawn: false,
-                },
-            },
-            scripts: {
-                files: ['src/*.js'],
-                tasks: ['uglify'],
-                options: {
-                    spawn: false,
-                },
-            }
+  grunt.initConfig({
+    // LESS para CSS
+    less: {
+      development: {
+        files: {
+          'dist/styles.css': 'src/styles.less'
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Minificação do CSS
+    cssmin: {
+      target: {
+        files: {
+          'dist/styles.min.css': ['dist/styles.css']
+        }
+      }
+    },
 
-    grunt.registerTask('default', ['less', 'cssmin', 'uglify']);
-    grunt.registerTask('dev', ['watch']);
+    // Minificação JS
+    uglify: {
+      build: {
+        src: 'src/scripts.js',
+        dest: 'dist/scripts.min.js'
+      }
+    },
+
+    // Watch
+    watch: {
+      styles: {
+        files: ['src/*.less'],
+        tasks: ['less', 'cssmin'],
+        options: {
+          spawn: false,
+        },
+      },
+      scripts: {
+        files: ['src/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      }
+    }
+  });
+
+  // Carregar plugins
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Tarefa padrão
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify']);
+
+  // Tarefa de desenvolvimento (watch)
+  grunt.registerTask('dev', ['watch']);
 };
